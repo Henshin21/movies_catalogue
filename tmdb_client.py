3942c71f840988b7a92020625777fdf41a4d51e6
Tai Cao <Henshin21@yahoo.pl>
2023-01-28 16:53:02 +0100

    added client py

---
+++ tmdb_client.py
@@ -0,0 +1,14 @@
+import requests
+
+def get_popular_movies():
+    endpoint = "https://api.themoviedb.org/3/movie/popular"
+    api_token = "twoj-token"
+    headers = {
+        "Authorization": f"Bearer {api_token}"
+    }
+    response = requests.get(endpoint, headers=headers)
+    return response.json()
+
+def get_poster_url(poster_api_path, size="w342"):
+    base_url = "https://image.tmdb.org/t/p/"
+    return f"{base_url}{size}/{poster_api_path}"
