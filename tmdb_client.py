ddf8d8632f99d6d682dac9ec297243f277ee2a77
Tai Cao <Henshin21@yahoo.pl>
2023-01-29 23:16:48 +0100

    added movies db and api into it

--- tmdb_client.py
+++ tmdb_client.py
@@ -1,8 +1,8 @@
 import requests
 
 def get_popular_movies():
-    endpoint = "https://api.themoviedb.org/3/movie/popular"
-    api_token = "twoj-token"
+    endpoint = "https://api.themoviedb.org/3/movie/popular?api_key=c98f13140ddb6dd3cda199eb01c083cf"
+    api_token = "c98f13140ddb6dd3cda199eb01c083cf"
     headers = {
         "Authorization": f"Bearer {api_token}"
     }
@@ -12,3 +12,7 @@ def get_popular_movies():
 def get_poster_url(poster_api_path, size="w342"):
     base_url = "https://image.tmdb.org/t/p/"
     return f"{base_url}{size}/{poster_api_path}"
+
+def get_movies(how_many):
+    data = get_popular_movies()
+    return data["results"][:how_many]
