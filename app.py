7f9e3e9ec869abcfe7f4e0c818f616c7a4db7b37
Tai Cao <Henshin21@yahoo.pl>
2023-01-30 16:22:31 +0100

    added new app routes

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 from flask import Flask, render_template
-
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static", static_url_path="/static")
+import tmdb_client
 
 @app.route('/')
-def index():
-    movies = []
-    return render_template("index.html")
-
-@app.route('/homepage')
 def homepage():
-    movies = []
+    movies = tmdb_client.get_popular_movies()["results"][:8]
     return render_template("homepage.html", movies=movies)
+
+@app.context_processor
+def utility_processor():
+    def tmdb_image_url(path, size):
+        return tmdb_client.get_poster_url(path, size)
+    return {"tmdb_image_url": tmdb_image_url}
+
+@app.route("/movie/<movie_id>")
+def movie_details(movie_id):
+    details = tmdb_client.get_single_movie(movie_id)
+    return render_template("movie_details.html", movie=details)
